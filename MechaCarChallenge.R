96a7f4f906376fac590e77f4aa6ab26668118f8e
dflemi15 <107585908+dflemi15@users.noreply.github.com>
2022-09-25 08:47:44 -0400

    Delete MechaCarChallenge.R

--- MechaCarChallenge.R
+++
@@ -1,9 +0,0 @@
-library(ggplot2)
-library(magrittr)
-library(dplyr)
-library(tidyr)
-mechacar_df <- read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactors = F)
-lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + 
-     ground_clearance + AWD,data=mechacar_df)
-summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + 
-             ground_clearance + AWD,data=mechacar_df)) #summarize linear model
