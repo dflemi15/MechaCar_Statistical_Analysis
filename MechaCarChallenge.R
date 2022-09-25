50c4b5b9a111e3a42b3cae005732cdd93cdcf194
dflemi15 <107585908+dflemi15@users.noreply.github.com>
2022-09-25 08:48:17 -0400

    Add files via upload

---
+++ MechaCarChallenge.R
@@ -0,0 +1,28 @@
+library(ggplot2)
+library(magrittr)
+library(dplyr)
+library(tidyr)
+
+# Deliverable 1
+mechacar_df <- read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactors = F)
+lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + 
+     ground_clearance + AWD,data=mechacar_df)
+summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + 
+             ground_clearance + AWD,data=mechacar_df)) #summarize linear model
+
+# Deliverable 2
+suspension_df <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFactors = F)
+head(suspension_df)
+
+Mean = mean(suspension_df$PSI)
+Median=median(suspension_df$PSI)
+Variance=var(suspension_df$PSI)
+SD = sd(suspension_df$PSI)
+
+total_summary <- data.frame(Mean, Median, Variance, SD)
+
+lot_summary <- suspension_df %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI), 
+                                                                           Variance=var(PSI), SD=sd(PSI), .groups = 'keep')
+
+
+
